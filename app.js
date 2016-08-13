aa6be02b1ba5819df26bca4527824521b324f731
do1tgood <yuriy.stryzhekozin@gmail.com>
2016-08-14 01:23:57 +0300

    Send hello message and emit user messages

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
-// const app = require('express')(),
-//       http = require("http").Server(app),
-//       cors = require('cors'),
-//       io = require('socket.io')(http, {
-//         origins: "localhost:3000"
-//       });
-
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-// });
-
-// http.listen(8000, function(){
-//   console.log('listening on *:8000');
-// });
-var app = require('express');
-var server = require('http').createServer(app).listen(8888);
-var io = require('socket.io')(server, {
-  origins: "localhost:3000"
-});
-
-// app.listen(8888);
-
-// app.get('/', function (req, res) {
-//   res.sendfile(__dirname + '/index.html');
-// });
+var app = require('express'),
+    server = require('http').createServer(app).listen(8888),
+    io = require('socket.io')(server, {
+      origins: "localhost:3000"
+    });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.emit("chat message", {
+    message: "Chat or die"
+  });
+
+  socket.on("chat message", function (data) {
+    socket.emit("chat message", data);
   });
 });
